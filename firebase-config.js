3fdf6f90b08915ca16e2a131e863c03f4bbbe5d9
diogomgf-cmd <diogomgf@gmail.com>
2026-07-22 22:36:50 +0100

    Update Firebase config

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,11 @@
 const firebaseConfig = {
-  apiKey: "COLOA_AQUI_API_KEY",
-  authDomain: "SEU_PROJETO.firebaseapp.com",
-  projectId: "SEU_PROJETO",
-  storageBucket: "SEU_PROJETO.appspot.com",
-  messagingSenderId: "COLOA_AQUI_SENDER_ID",
-  appId: "COLOA_AQUI_APP_ID"
+  apiKey: "AIzaSyCL5KB4TOBHpLp59Es8AUtGUQN33QsXEd0",
+  authDomain: "drfm-app.firebaseapp.com",
+  projectId: "drfm-app",
+  storageBucket: "drfm-app.firebasestorage.app",
+  messagingSenderId: "883562163504",
+  appId: "1:883562163504:web:1c201b1c4d963f38e7ecf5",
+  measurementId: "G-PL7YD2S4T0"
 };
 
 const FIREBASE_ENABLED =
